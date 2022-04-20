dd103469161857715d533ec82a879e6f7721a431
Adam Lay <adam93.lay+git@gmail.com>
2022-04-20 16:19:05 +0100

    UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,20 +15,11 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <WordGrid />
+        <ol style={{color:"white"}}>
+          <li>Type in your guessed words</li>
+          <li>Click on the letters to cycle through Absent/Present/Correct</li>
+        </ol>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
